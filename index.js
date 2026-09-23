8655c8aea2fbaa354cf82365b941a571c17b94cb
yuri-ramos01 <yuriramos_01@outlook.com>
2026-09-22 22:26:14 -0300

    atualiza código

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Olá Mundão");
+let nome = "Maximus"
+let xp = 15000
+
+if (xp <= 1000)
+{console.log(nome + " está no nível " + "ferro")}
+else if (xp >= 1001  && xp <= 2000){
+    console.log(nome + " está no nível " + "BRONZE")
+}
+else if (xp >= 2001 && xp <= 5000){
+    console.log(nome + " está no nível " + "PRATA")
+}
+else if (xp >= 5001 && xp <= 7000){
+    console.log(nome + " está no nível " + "OURO")
+}
+else if (xp >= 7001 && xp <= 8000){
+    console.log(nome + " está no nível " + "PLATINA")
+}
+else if (xp >= 8001 && xp <= 9000){
+    console.log(nome + " está no nível " + "ASCENDENTE")
+}
+else if (xp >= 9001 && xp <= 10000){
+    console.log(nome + " está no nível " + "IMORTAL")
+}
+else {
+    console.log(nome + " está no nível " + "RADIANTE")
+}
+
+
